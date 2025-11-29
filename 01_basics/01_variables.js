2233c64f2ef801609d4fd29a5adcc4bc499547e0
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-11-29 05:53:48 +0000

    let const var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,33 @@
+const accountId =  144553
+let accountEmail = "kaustubhcet2020@gmail.com"
+var accountPassword =  "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+console.log(accountId);
+accountEmail = "kauce@gmail.com"
+accountPassword = "2121212"
+accountCity = "MUMBAI"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+console.log(typeof accountCity);
+
+
+// DIFFERENCE BETWEEN LET AND VAR
+// {
+//     var name = "kaustubh"
+//     var name = "KS"
+//     var name = "mhatre"
+// }
+// console.log(name);
+
+// {
+//     let age = 11
+//     let age = 12
+//     let age = 13
+// }
+// console.log(age);
+
+
