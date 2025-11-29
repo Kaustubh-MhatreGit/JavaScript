59c1a696951d8b5d2296598014db0a2097392562
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-11-29 16:14:41 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,58 @@
+// # Primitive (7 types)
+/*
+String
+Number
+Boolean
+null
+undefined
+Symbol
+BigInt
+*/
+
+
+// # Non-Primitive
+/*
+Arrays,
+Objects,
+Functions
+*/
+
+/*
+const score = 100
+const value = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+// SYMBOL
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+// BigInt
+const bigNumber = 3255846164198494984984649494n
+console.log(typeof bigNumber);
+*/
+
+const heroes = ["raj", "kaj", "paj"];
+
+let myobj = {
+    name: "Kaustubh",
+    age : 22
+}
+
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunction);
+console.log(typeof  myobj);
+console.log(typeof  heroes);
+
+
+
+
+
+
