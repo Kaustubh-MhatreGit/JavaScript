c90ec96b27e0f3e8047b7afaf24e22774234e874
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-12-03 15:17:51 +0000

    Functions with objects and array in javascript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -53,5 +53,38 @@ console.log(loginUserMessage())
 
 ////////////////////////////////////////////////////////////////////
 
+// function calculateCartPrice(...num1){   use alternate
+function calculateCartPrice(val1, val2, ...num1){
+     return num1
+}
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+/////////////////////////////////////////////////////////////
+
+const user ={
+    username : "hitesh",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user)     //switch alternate
+
+// handleObject({
+//     username : "sam",
+//     price : 99
+// })
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
+
+
 
 
