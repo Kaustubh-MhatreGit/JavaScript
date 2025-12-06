9350345055675bb1c12a1fc5be68ef9eed68acd7
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-12-06 06:54:51 +0000

    Scope level and mini hoisting in javascript

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,11 +1,55 @@
-const b = 300
+// // const b = 300
 
-if(true){
-    let a = 10;
-    const b = 20;
-    console.log(b);     
+// // if(true){
+// //     let a = 10;
+// //     const b = 20;
+// //     console.log(b);     
+// // }
+// // console.log(b);
+
+// ////////////////NESTED SCOPE////////////////////
+// // function one(){
+// //     const username = "Hitesh"
+
+// //     function two() {
+// //         const website = "Youtube"
+// //         console.log(username);   
+// //     }
+
+// //     // console.log(website);
+
+// //     two()
+
+// // }
+// // one()
+
+// /////////////////////////IF ELSE//////////////
+// if (true) {
+//     const username = "hitesh"
+//     if (username === "hitesh") {
+//         const website = "youtube"
+//         console.log(username + website);
+
+//     }
+//     // console.log(website);  error
+// }
+// // console.log(username);  error
+
+
+
+//////////////////INTERESTING///////////////////
+///////////////HOISTING/////////////////////
+console.log(addone(6));
+function addone(num){
+    return num + 1
 }
-console.log(b);
+
+
+console.log(addTwo(6));
+const addTwo = function(num){
+    return num + 2
+}
+
 
 
 
