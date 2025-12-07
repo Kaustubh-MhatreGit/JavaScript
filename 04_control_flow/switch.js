1e92ff641a65a51c1f91f8ae55bce326f3efde0f
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-12-07 07:23:53 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,70 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+
+    case 2:
+        console.log("Feb");
+        break;
+
+    case 3:
+        console.log("march");
+        break;
+
+    case 4:
+        console.log("april");
+        break;
+
+    case 5:
+        console.log("may");
+        break;
+
+    case 6:
+        console.log("june");
+        break;
+    default:
+        console.log("DEC");
+        
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+const months = "march"
+switch (months) {
+    case "jan":
+        console.log("january");
+        break;
+
+    case "feb":
+        console.log("Feb");
+        break;
+
+    case "march":
+        console.log("march");
+        break;
+
+    case "april":
+        console.log("april");
+        break;
+
+    case "May":
+        console.log("may");
+        break;
+
+    case "june":
+        console.log("june");
+        break;
+    default:
+        console.log("DEC");
+        
+}
