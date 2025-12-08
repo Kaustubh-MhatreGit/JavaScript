1f87947f258699c1e486e801c5e7bdb58776d15d
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-12-08 14:32:10 +0000

    map chaining as well as using map and filter together

---
+++ 05_iterations/seven.js
@@ -0,0 +1,34 @@
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newnums = myNumers.map( (num) => {return num + 10 } )
+// console.log(newnums);
+
+
+
+// const newnums = myNumers.map((num) => num + 10)
+// console.log(newnums);
+
+
+
+// const nums = []
+// myNumers.forEach( (num) => {
+//     if(num > 0){
+//         num = num + 10
+//         nums.push(num)
+//     }   
+// })
+// console.log(nums);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const number = myNumers.map( (num) => num * 10).map( (num) => num + 1).filter( (num) => num >=40 )
+console.log(number);
+
+
+
+
+
+
