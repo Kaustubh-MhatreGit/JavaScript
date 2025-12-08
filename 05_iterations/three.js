bae27d46987f1462d7ec9d158e53e87a885a652d
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-12-08 07:48:16 +0000

    FOR OF and MAP

---
+++ 05_iterations/three.js
@@ -0,0 +1,65 @@
+// for of loop
+
+// ['', '', '']
+// [{}, {}, {}]
+
+////////////////////////////////////////
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+//////////////////////////////////////////////
+
+// const greetings = "Hello World"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);   
+// }
+
+//////////////////////////////////////////////
+
+// const greetings = "Hello World"
+// for (const greet of greetings) {
+//     if (greet == " "){
+//         continue;
+//     }
+//     console.log(`Each char is ${greet}`);
+// }  self practice to remove space
+
+///////////////////////////////////////////////
+//MAPS
+// Only accepts unique values and show result in same sequence
+
+
+// const map = new Map()
+// map.set("IN", "INDIA")
+// map.set("USA", "UNITED STATES OF AMERICA")
+// map.set("FR", "France")
+
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(key);  // giving in array format
+// }
+
+/////////////////////////////////////////////////
+
+// for (const [key, value] of map) {
+//     console.log(key + ":" + value);  // giving in array format
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// const myObj = {
+//     "game1" : "nfs",
+//     "game2" : "Spiderman"
+// }
+
+// for (const [key, value] of myObj) {  
+//     console.log(key + ":" + value);
+// }
+
+// FOR OF DOESNT WORK FOR OBJECTS
+
