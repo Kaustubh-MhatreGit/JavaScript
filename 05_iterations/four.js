304a195196b2f4719e99a72b48bdd86ab686282d
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2025-12-08 07:48:35 +0000

    FOR IN

---
+++ 05_iterations/four.js
@@ -0,0 +1,52 @@
+const myObj = {
+    js : "JavaScript",
+    cpp : "C++",
+    rb : "Ruby",
+    swift : "Swift by Apple",
+}
+
+// for (const key in myObj) {
+//     console.log(`${key}`);    
+// }  GIVES KEYS
+
+// for (const key in myObj) {
+//     console.log(`${myObj[key]}`);   
+// } // GIVES VALUES
+
+
+// for (const key in myObj) {
+//     console.log(`${key} is the shortcut for ${myObj[key]}`);   
+// } 
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//        console.log(key);       
+// }
+
+
+
+// for (const key in programming) {  
+//     console.log(`for ${key} the language is ${programming[key]}`);   
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// const map = new Map()
+// map.set("IN", "INDIA")
+// map.set("USA", "UNITED STATES OF AMERICA")
+// map.set("FR", "France")
+
+// for (const key in map) {
+//     console.log(key);   
+// } 
+// FOR IN doesnt work on MAP
+// FOR OF works on MAP
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
