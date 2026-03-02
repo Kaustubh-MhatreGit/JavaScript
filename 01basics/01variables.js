7a5bccd1746ebe0253c8d3a1ad5fa2aec8767757
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2026-03-02 10:52:51 +0000

    Variables

---
+++ 01basics/01variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "kaustubhcet2020@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2
+console.log(accountId);
+
+accountEmail = "mhatre"
+accountPassword = "212121212"
+accountCity = "mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+    
