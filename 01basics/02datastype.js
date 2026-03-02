8092c00563c0776c2b5c6e0dc32116b94ecc00c3
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2026-03-02 12:10:45 +0000

    Datatypes

---
+++ 01basics/02datastype.js
@@ -0,0 +1,22 @@
+console.log(3+3)
+console.log(111);
+
+let name = "kaustubh"
+let age = 23
+let isLoggedIn = true
+let state = null
+
+/*
+number
+bigInt
+string
+boolean
+null
+undefined
+symbol
+
+object
+
+*/
+
+console.log(typeof undefined);
