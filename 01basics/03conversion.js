e5c80a91de46f56384b92ac67fc175482055b66e
Kaustubh-MhatreGit <kaustubhmaingit@gmail.com>
2026-03-17 15:30:29 +0000

    Update

---
+++ 01basics/03conversion.js
@@ -0,0 +1,33 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = false
+
+// let score = "Kaustubh"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1 false => 0
+
+//////////////////////////////////////////////////////////////
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = "Kaustubh"
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+//"" => false
+//"kaustubh" => true
+
+
